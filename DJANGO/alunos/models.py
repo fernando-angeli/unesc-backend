ecd942e99bd4e108458740844bba5e866e2beeb5
Fernando <fernando_angeli@hotmail.com>
2023-03-21 22:43:27 -0300

    Excluindo DJANGO

--- DJANGO/alunos/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Aluno(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.CharField(max_length=50)
-    matricula = models.CharField(max_length=20)
